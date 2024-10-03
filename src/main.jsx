47c18284a41fb557f4022fec77f5df8ffb17d615
Levi Francis <levifrancis15@gmail.com>
2024-10-03 03:24:18 +0100

    add app context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Theme } from "@radix-ui/themes";
+import { AppProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Theme>
-            <App />
+            <AppProvider>
+                <App />
+            </AppProvider>
         </Theme>
     </StrictMode>
 );
